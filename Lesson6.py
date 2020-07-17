cc2431ea3fc262f9bed0c112178ac045d1ab2867
Udemezue John <johnzeus14@gmail.com>
2020-07-17 08:35:30 +0100

    added Lesson 6

---
+++ Lesson6.py
@@ -0,0 +1,72 @@
+'''
+LIST.
+
+A list is collection which is ordered and changeable and allows duplicate members.
+
+
+
+'''
+
+# clubs = ["Arsenal", "Man u", "Barcelona", "Liverpool", "Chelsea", "Real Madrid"]
+
+'''
+
+
+Indexing  (positive and negative indexing)
+
+'''
+
+# print(clubs[0])
+
+# print(clubs[1])
+
+# print (clubs[-1])
+
+# print (clubs[-2])
+
+# clubs = ["Arsenal", "Man u", "Barcelona", "Liverpool", "Chelsea", "Real Madrid"]
+
+'''
+
+List operation
+
+append() To add an item to the end of the list
+Insert() To add an Item to the specified index
+remove() Removes the specified Item from the list.
+pop() removes the last item from the list
+
+
+'''
+
+# clubs.append("Mancity")
+# clubs.append("Bayern Munchen")
+
+# clubs.insert(4, "Leicerster")
+# clubs.insert(2, "Shaktar Donesk")
+
+# clubs.remove("Barcelona")
+
+# clubs.remove("Chelsea")
+
+# clubs.pop()
+
+# print(clubs)
+
+clubs = ["Arsenal", "Man u", "Barcelona", "Liverpool", "Chelsea", "Real Madrid"]
+
+'''
+
+For Loops
+
+List, Dictionary and Tuples are known as sequence
+
+'''
+
+for b in clubs:
+
+	print(b)
+
+
+
+
+
