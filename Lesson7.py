024f3f127c0b6b5fc069fdfba19f30dea846a75a
Udemezue John <johnzeus14@gmail.com>
2020-07-18 07:37:53 +0100

    added lesson 7

---
+++ Lesson7.py
@@ -0,0 +1,44 @@
+'''
+
+A Tuple is a collection which is ordered and unchangeable.
+
+Once a tuple is created, you cannot change its values. 
+
+Tuples are unchangeable, or immutable as it also is called.
+
+'''
+
+clubs  = ("Arsenal", "chelsea", "Liverpool", "Man U")
+
+#Indexing
+
+# print(clubs[0])
+
+
+# print(clubs[1])
+
+# print(clubs[-1])
+
+'''
+
+Looping through a Tuple
+
+'''
+for club in clubs:
+
+	print(club)
+
+'''
+
+checking if an Item exist in a Tuple
+
+'''
+
+if "Arsenal" in clubs:
+
+	print("Arsenal is one of the big four")
+
+
+
+
+
